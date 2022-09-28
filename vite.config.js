b146bb0f65373b24c737b1ea6513eca72c96b6da
Tom Goral <tg@tomgoral.com>
2022-09-28 10:24:04 -0400

    add card game app

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'src/pages/code.html')
+      }
+    }
+  }
+})
